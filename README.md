db6625659bb2441619eba14480a23cf05801786b
Piotr Jastrzebski <piotr@chiselstrike.com>
2023-03-05 10:34:58 +0100

    Enable Websockets support
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Running examples
 
+Using HTTPS:
 ```console
 go run -ldflags="-X 'main.dbUrl=https://<login>:<password>@<sqld db url>'" examples/sql-driver/counter/main.go
+```
+
+Using Websockets:
+```console
+export SQLD_AUTH_TOKEN=<jwt authorization token>
+go run -ldflags="-X 'main.dbUrl=wss://<sqld db url>:2023'" examples/sql-driver/counter/main.go
 ```
--- sql_driver/lib.go
+++ sql_driver/lib.go
@@ -3,9 +3,12 @@ package libsqldriver
 import (
 	"database/sql"
 	"database/sql/driver"
+	"fmt"
+	"os"
 	"strings"
 
 	"github.com/libsql/libsql-client-go/internal/sqld/sqldhttp"
+	"github.com/libsql/libsql-client-go/internal/sqld/sqldwebsockets"
 	"github.com/mattn/go-sqlite3"
 )
 
@@ -16,7 +19,17 @@ func (d *LibsqlDriver) Open(dbPath string) (driver.Conn, error) {
 	if strings.HasPrefix(dbPath, "file:") {
 		return (&sqlite3.SQLiteDriver{}).Open(dbPath)
 	}
-	return sqldhttp.SqldConnect(dbPath), nil
+	if strings.HasPrefix(dbPath, "wss://") {
+		jwt := os.Getenv("SQLD_AUTH_TOKEN")
+		if len(jwt) == 0 {
+			return nil, fmt.Errorf("missing authorization token. Please provide JWT by setting SQLD_AUTH_TOKEN env variable")
+		}
+		return sqldwebsockets.SqldConnect(dbPath, jwt)
+	}
+	if strings.HasPrefix(dbPath, "https://") {
+		return sqldhttp.SqldConnect(dbPath), nil
+	}
+	return nil, fmt.Errorf("unsupported db path: %s\nThis driver supports only db paths that start with file://, https:// or wss://", dbPath)
 }
 
 func init() {
