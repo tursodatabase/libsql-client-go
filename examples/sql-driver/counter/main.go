fd794bcc0596465ddbaf9666e1a973256e630e03
Piotr Jastrzebski <piotr@chiselstrike.com>
2023-03-05 11:38:34 +0100

    Add example of concurrent users
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- examples/sql-driver/counter/main.go
+++ examples/sql-driver/counter/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"sync"
 
 	_ "github.com/libsql/libsql-client-go/sql_driver"
 )
@@ -66,10 +67,97 @@ func runCounterExample(dbPath string) {
 	}
 }
 
+func runConcurrentExample(dbPath string) {
+	db, err := sql.Open("libsql", dbPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open db %s: %s", dbPath, err)
+		os.Exit(1)
+	}
+	exec(db, "DROP TABLE IF EXISTS table1")
+	exec(db, "DROP TABLE IF EXISTS table2")
+	exec(db, "DROP TABLE IF EXISTS table3")
+	exec(db, "CREATE TABLE table1(key int, value int)")
+	exec(db, "CREATE TABLE table2(key int, value int)")
+	exec(db, "CREATE TABLE table3(key int, value int)")
+	for i := 1; i < 10; i++ {
+		exec(db, "INSERT INTO table1 VALUES(?, ?)", i, i)
+		exec(db, "INSERT INTO table2 VALUES(?, ?)", i, -1*i)
+		exec(db, "INSERT INTO table3 VALUES(?, ?)", i, 0)
+	}
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		for i := 1; i < 100; i++ {
+			rows := query(db, "SELECT value FROM table1")
+			for rows.Next() {
+				var v int
+				if err := rows.Scan(&v); err != nil {
+					fmt.Fprintf(os.Stderr, "failed to scan row: %s", err)
+					os.Exit(1)
+				}
+				if v <= 0 {
+					fmt.Fprintf(os.Stderr, "got non-positive value from table1: %d", v)
+					os.Exit(1)
+				}
+			}
+			if err := rows.Err(); err != nil {
+				fmt.Fprintf(os.Stderr, "errors from query: %s", err)
+				os.Exit(1)
+			}
+		}
+	}(&wg)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		for i := 1; i < 100; i++ {
+			rows := query(db, "SELECT value FROM table2")
+			for rows.Next() {
+				var v int
+				if err := rows.Scan(&v); err != nil {
+					fmt.Fprintf(os.Stderr, "failed to scan row: %s", err)
+					os.Exit(1)
+				}
+				if v >= 0 {
+					fmt.Fprintf(os.Stderr, "got non-negative value from table2: %d", v)
+					os.Exit(1)
+				}
+			}
+			if err := rows.Err(); err != nil {
+				fmt.Fprintf(os.Stderr, "errors from query: %s", err)
+				os.Exit(1)
+			}
+		}
+	}(&wg)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		for i := 1; i < 100; i++ {
+			rows := query(db, "SELECT value FROM table3")
+			for rows.Next() {
+				var v int
+				if err := rows.Scan(&v); err != nil {
+					fmt.Fprintf(os.Stderr, "failed to scan row: %s", err)
+					os.Exit(1)
+				}
+				if v != 0 {
+					fmt.Fprintf(os.Stderr, "got non-zero value from table3: %d", v)
+					os.Exit(1)
+				}
+			}
+			if err := rows.Err(); err != nil {
+				fmt.Fprintf(os.Stderr, "errors from query: %s", err)
+				os.Exit(1)
+			}
+		}
+	}(&wg)
+	wg.Wait()
+}
+
 var dbUrl = ""
 var dbFile = "file:test.db"
 
 func main() {
 	runCounterExample(dbUrl)
 	runCounterExample(dbFile)
+	runConcurrentExample(dbUrl)
+	runConcurrentExample(dbFile)
 }
